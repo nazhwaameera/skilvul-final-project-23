56f2901ddadc4b651640b8c0c0612b48db96a01b
revydo <eldodo641@gmail.com>
2022-01-12 18:50:01 +0700

    cloudinary

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import Uploader from "../components/Upload";
 
 export default function Maps() {
+  const [fileSelected, setFileSelected] = useState("");
+
+  const upload = () => {
+    const formData = new FormData();
+    formData.append("file", fileSelected);
+    formData.append("upload_preset", "remedial");
+
+    Axios.post("https://api.cloudinary.com/v1_1/dcuyi8m12/image/upload", formData).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <Container className="mt-5 bg-secondary">
       <p>&nbsp;</p>
@@ -37,6 +50,19 @@ export default function Maps() {
           <Col>
             <h3>QUEST COMPLETION</h3>
             <p className="my-1">Maksimal ukuran file 100MB, hanya menerima satu (1) tipe file untuk satu quest</p>
+            <input
+              type="file"
+              onChange={(event) => {
+                setFileSelected(event.target.files[0]);
+              }}
+            />
+            <button
+              onClick={() => {
+                upload();
+              }}
+            >
+              Upload File
+            </button>
             <p className="my-2">
               <b>QUEST SAAT INI:</b> Jelaskan tentang hal yang menarik perhatianmu belakangan ini
             </p>
