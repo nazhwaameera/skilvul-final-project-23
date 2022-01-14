c3599b504c666b5fc7a568a914cdc4336637b120
Nazhwa Ameera <68385532+nazhwaameera@users.noreply.github.com>
2022-01-14 15:18:57 +0700

    commit lagi ada yang ketinggalan ahahah

--- routes/peserta.js
+++ routes/peserta.js
@@ -5,12 +5,14 @@ const PesertaController = require("../controllers/peserta.controller");
 
 router.post("/login", PesertaController.loginPeserta);
 router.get("/profil/:nama", PesertaController.lihatProfil);
-// router.get("/mentoring", PesertaController.mentoring);
-// router.get("/feedbacks", PesertaController.lihatFeedbacksbyID);
-// router.get("/profil/:nama", PesertaController.lihatAchievementsbyID);
-// router.get("/todos", PesertaController.lihatTodosbyID);
+router.get("/mentoring", PesertaController.mentoring);
+router.get("/feedbacks", PesertaController.lihatFeedbacksbyID);
+router.get("/profil/:nama", PesertaController.lihatAchievementsbyID);
+router.get("/todos", PesertaController.lihatTodosbyID);
 router.post("/todos/add-todo", PesertaController.createTodo);
 router.delete("/todos/:id_todo", PesertaController.deleteTodo);
 router.get("/todos/get-all", PesertaController.getAllTodos);
+router.get("/maps/:id_maps", PesertaController.getActiveMap);
+router.post("/maps/:id_maps/quests/:id_quest", PesertaController.menyelesaikanQuest);
 
 module.exports = router;
