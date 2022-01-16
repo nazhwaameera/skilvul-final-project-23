80d2eb38279e1b14e07573479d0b63c06e56467c
Nazhwa Ameera <68385532+nazhwaameera@users.noreply.github.com>
2022-01-16 13:15:33 +0700

    Update detail penyelesaian

--- controllers/mentor.controller.js
+++ controllers/mentor.controller.js
@@ -87,12 +87,18 @@ class MentorController {
   static async detailPenyelesaian(req, res) {
     try {
         const id_quest = req.params.id_quest;
-        const detailPenyelesaian = await Quest.find({ _id: id_quest}).populate([
+        const detailPenyelesaian = await Quest.find({ _id: id_quest})
+        .populate([
+          {
+            path: 'id_peserta',
+            model: 'Peserta'
+          },
+        ]).populate([
           {
             path: 'files',
             model: 'File'
           },
-        ]);
+        ])
         res.status(200).send(detailPenyelesaian);
     } catch (error){
         res.status(500).send({ err: error.message });
@@ -141,26 +147,26 @@ class MentorController {
   }
 
   static async createQuest(req, res) {
-    try {
-        const body = req.body;
-        const id_peserta = body.id_peserta;
-        const id_mentor = body.id_mentor;
-        const id_maps = body.id_maps;
-        const konten = body.konten;
+      const body = req.body;
+      const id_peserta = body.id_peserta;
+      const id_mentor = body.id_mentor;
+      const id_maps = body.id_maps;
+      const konten = body.konten;
   
-        const questContent = new Quest({
-          konten: konten,
-          id_peserta: id_peserta,
-          id_mentor: id_mentor,
-          id_maps: id_maps
-        });
-  
-        const saved = await questContent.save(async(err, quest) => {
+      const questContent = new Quest({
+        konten: konten,
+        id_peserta: id_peserta,
+        id_mentor: id_mentor,
+        id_maps: id_maps
+      });
+
+      try {
+        await questContent.save(async(err, quest) => {
           await Map.findOneAndUpdate({ _id: req.body.id_maps }, { $addToSet: { quests: quest._id } }, { new: true });
           await Peserta.findOneAndUpdate({ _id: req.body.id_peserta }, { $addToSet: { quests: quest._id } }, { new: true });
         });
-        console.log(saved);
-        res.status(201).send(saved);
+        console.log(questContent);
+        res.status(201).send(questContent);
       } catch (error) {
         res.status(500).send({ err: error });
         console.log(error)
